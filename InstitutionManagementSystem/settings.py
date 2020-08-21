051bd0a99e99c793eab681fd6fcc120ed430e2ca
toms joseph <tomsjoseph.t6@gmail.com>
2020-08-21 14:09:28 +0530

    try2

--- InstitutionManagementSystem/settings.py
+++ InstitutionManagementSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,5 +155,4 @@ EMAIL_HOST_USER = 'email not set'
 EMAIL_HOST_PASSWORD = 'email not set'
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
